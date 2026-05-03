c01781679965249edf9b5b27d4d18285314074f1
syedamariumhasnain <syedamarium27@gmail.com>
2026-05-03 15:10:43 +0500

    icons, fonts, theme colors -- integrated

---
+++ image.d.ts
@@ -0,0 +1,23 @@
+// Defining rule for images ---
+// Exporting different png's, ... for type any
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
